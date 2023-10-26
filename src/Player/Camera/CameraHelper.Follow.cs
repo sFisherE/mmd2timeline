8845fa46c89431ec560ba0fce5d50834fc1b265a
noone77521 <jame.xuxu@gmail.com>
2023-10-27 01:29:27 +0800

    修正无镜头数据时镜头自由视角被禁用的问题

--- src/Player/Camera/CameraHelper.Follow.cs
+++ src/Player/Camera/CameraHelper.Follow.cs
@@ -282,8 +282,8 @@ namespace mmd2timeline
         /// <param name="disable"></param>
         internal void DisableNavigation(bool disable = true)
         {
-            // 未启用镜头，跳过处理
-            if (!config.EnableCamera || !HasMotion)
+            // 未启用镜头时，如果要禁用镜头，跳过处理
+            if (disable && (!config.EnableCamera || !HasMotion))
                 return;
 
             if (config.UseWindowCamera)
