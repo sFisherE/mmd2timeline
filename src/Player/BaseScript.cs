2f4fd2477cd724cef2a5a944f41c264cc476b665
noone77521 <jame.xuxu@gmail.com>
2023-11-12 21:28:10 +0800

    更改测试本版号

--- src/Player/BaseScript.cs
+++ src/Player/BaseScript.cs
@@ -13,7 +13,7 @@ namespace mmd2timeline
     {
         public const string PLUGIN_NAME = "MMD2TimelinePlayer";
 
-        public const string VERSION = "1.0f1c";
+        public const string VERSION = "1.0f1d";
 
         /// <summary>
         /// 获取插件的版本号
